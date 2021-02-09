e0189c6f85489f189984e57812327c84827cc362
Berk Elmas <42638691+berkelmas@users.noreply.github.com>
2021-02-09 11:45:00 +0300

    Update prob_calculator.py
    no need for an instance in an exported class file.

--- prob_calculator.py
+++ prob_calculator.py
@@ -41,15 +41,3 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
         
     probability = nominator / denominator
     return probability
-
-
-hat = Hat(blue=3, red=2, green=6)
-probability = experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "green": 1},
-    num_balls_drawn=4,
-    num_experiments=1000)
-print("Probability:", probability)
-hat.draw(2)
-print(len(hat.contents))
